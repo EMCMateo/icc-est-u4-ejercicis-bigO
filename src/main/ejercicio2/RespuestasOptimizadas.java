dbfbe46f3d5cc01c11c9565ba8aa334c7ba15ed4
EMCMateo <mateo3456moli@gmail.com>
2025-05-20 08:53:15 -0500

    commit

--- src/main/ejercicio2/RespuestasOptimizadas.java
+++ src/main/ejercicio2/RespuestasOptimizadas.java
@@ -1,30 +1,58 @@
 package main.ejercicio2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RespuestasOptimizadas {
     // El estudiante debe escribir aquí las versiones optimizadas y las
     // complejidades
 
     // 1. Versión optimizada de tieneDuplicados (Complejidad anotada en la variable)
-    public static String tieneDuplicadosComplejidad = ""; // Ej: "O(n)"
-
+    public static String tieneDuplicadosComplejidad = "O(n)";
     public static boolean tieneDuplicadosOpt(int[] arr) {
-        // TODO: implementar versión eficiente
-        return false;
+    Set<Integer> vistos = new HashSet<>();
+    for (int n : arr) {
+        if (vistos.contains(n)) {
+            return true;
+        }
+        vistos.add(n);
+    }
+    return false;
     }
 
     // 2. Versión optimizada de contarMayores (Complejidad anotada en la variable)
     public static String contarMayoresComplejidad = ""; // Ej: "O(n)"
 
     public static int contarMayoresOpt(int[] arr, int x) {
-        // TODO: implementar versión eficiente
-        return 0;
+        int count = 0;
+        int maxSoFar = Integer.MIN_VALUE;   
+
+        for (int value : arr) {
+            if (value > x && value >= maxSoFar) {
+            count++;
+            }
+            if (value > maxSoFar) {
+            maxSoFar = value;
+            }
+        }
+
+    return count;
     }
 
     // 3. Versión optimizada de encontrarMaximo (Complejidad anotada en la variable)
     public static String encontrarMaximoComplejidad = ""; // Ej: "O(n)"
 
     public static int encontrarMaximoOpt(int[] arr) {
-        // TODO: implementar versión eficiente
-        return 0;
+         int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] > arr[i])
+                    esMaximo = false;
+            }
+            if (esMaximo)
+                max = arr[i];
+        }
+        return max;
     }
 }
